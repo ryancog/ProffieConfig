9b692914deef48d79bcdc221495f3558bfd0c7ac
ryancog <170381220+ryancog@users.noreply.github.com>
2023-09-27 18:38:08 -0500

    Changed ProffieOS to submodule

--- build-Windows/resources/ProffieOS/pov_tools/create_POV_data_files-Win.bat
+++
@@ -1,1 +0,0 @@
-start powershell -noexit -command "bash create_POV_data_files"
--- build-Windows/resources/ProffieOS/props/saber_fett263_buttons.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d8075242657081c8a3b0fde081a5a66fa55670c09b4a23f7d98f08e1d202210e
-size 204385
