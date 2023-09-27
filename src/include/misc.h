60478c88929246e5180ff86f792a86e32e3dfd34
ryancog <170381220+ryancog@users.noreply.github.com>
2023-09-27 17:31:57 -0500

    Updated misc enum for macOS
    Menu item ID cannot be 0 on mac, so order was changed to make
    `ID_Initialize` > 0

--- src/include/misc.h
+++ src/include/misc.h
@@ -10,48 +10,48 @@
 class Misc
 {
 public:
-  enum {
-    ID_Initialize,
-    ID_WindowSelect,
-    ID_RefreshDevButton,
-    ID_ApplyButton,
-    ID_DevSelect,
-    ID_PropSelect,
-    ID_PropOption,
-    ID_GenFile,
-    ID_PresetList,
-    ID_BladeList,
-    ID_PresetEditor,
-    ID_AddPreset,
-    ID_RemovePreset,
-    ID_PresetName,
-    ID_PresetDir,
-    ID_PresetTrack,
-    ID_BladeSelect,
-    ID_SubBladeSelect,
-    ID_AddBlade,
-    ID_RemoveBlade,
-    ID_AddSubBlade,
-    ID_RemoveSubBlade,
-    ID_BladeType
-  };
+    enum {
+        ID_WindowSelect,
+        ID_Initialize,
+        ID_RefreshDevButton,
+        ID_ApplyButton,
+        ID_DevSelect,
+        ID_PropSelect,
+        ID_PropOption,
+        ID_GenFile,
+        ID_PresetList,
+        ID_BladeList,
+        ID_PresetEditor,
+        ID_AddPreset,
+        ID_RemovePreset,
+        ID_PresetName,
+        ID_PresetDir,
+        ID_PresetTrack,
+        ID_BladeSelect,
+        ID_SubBladeSelect,
+        ID_AddBlade,
+        ID_RemoveBlade,
+        ID_AddSubBlade,
+        ID_RemoveSubBlade,
+        ID_BladeType
+    };
 
-  struct numEntry {
-    wxBoxSizer *box{nullptr};
-    wxSpinCtrl *num{nullptr};
-  };
+    struct numEntry {
+        wxBoxSizer *box{nullptr};
+        wxSpinCtrl *num{nullptr};
+    };
 
-  struct numEntryDouble {
-    wxBoxSizer *box{nullptr};
-    wxSpinCtrlDouble *num{nullptr};
-  };
+    struct numEntryDouble {
+        wxBoxSizer *box{nullptr};
+        wxSpinCtrlDouble *num{nullptr};
+    };
 
-  static numEntry createNumEntry(wxStaticBoxSizer *parent, wxString displayText, int32_t ID, int32_t minVal, int32_t maxVal, int32_t defaultVal);
-  static numEntryDouble createNumEntryDouble(wxStaticBoxSizer *parent, wxString displayText, int32_t ID, double minVal, double maxVal, double defaultVal);
+    static numEntry createNumEntry(wxStaticBoxSizer *parent, wxString displayText, int32_t ID, int32_t minVal, int32_t maxVal, int32_t defaultVal);
+    static numEntryDouble createNumEntryDouble(wxStaticBoxSizer *parent, wxString displayText, int32_t ID, double minVal, double maxVal, double defaultVal);
 
-  static const wxArrayString createEntries(std::vector<std::string> list);
-  static const wxArrayString createEntries(std::initializer_list<std::string> list);
+    static const wxArrayString createEntries(std::vector<std::string> list);
+    static const wxArrayString createEntries(std::initializer_list<std::string> list);
 
 private:
-  Misc();
+    Misc();
 };
