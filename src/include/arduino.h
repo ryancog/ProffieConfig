607a29f574f3bf22e9443914ccc2e3d81cdd8a8d
ryancog <170381220+ryancog@users.noreply.github.com>
2023-10-29 19:05:06 -0500

    Fixed Windows Compile/Upload and Blade Config saving

--- src/include/arduino.h
+++ src/include/arduino.h
@@ -1,20 +1,22 @@
 #pragma once
-#include "mainwindow.h"
-
 #include <vector>
 #include <string>
 #include <wx/combobox.h>
 
 class Arduino {
 public:
-  static void refreshBoards(MainWindow*);
+  static void refreshBoards();
   static void updateList(wxComboBox*);
 
-  static void applyToBoard(MainWindow*);
+  static void applyToBoard();
+  static void verifyConfig();
+
   static void updateIno();
   static std::string compile();
   static std::string parseError(const std::string&);
   static std::string upload();
+
+  static FILE* CLI(const std::string& command);
 
   static void init();
   static std::vector<std::string> getBoards();
