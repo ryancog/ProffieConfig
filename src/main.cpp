d8ba46a216ebeff00e9b6ddf57fc73089ea17fba
ryancog <170381220+ryancog@users.noreply.github.com>
2023-11-25 16:37:09 -0600

    Fix for macOS to ensure consistent workdir for resource loading.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,23 @@
 #include "mainwindow.h"
 #include "configuration.h"
 
+#ifdef __WXOSX__
+#include <mach-o/dyld.h>
+#include <libgen.h>
+#endif
+
 class ProffieConfig : public wxApp {
 public:
   virtual bool OnInit() {
+
+#   ifdef __WXOSX__
+    char path[PATH_MAX];
+    uint32_t pathLen = sizeof(path);
+    _NSGetExecutablePath(path, &pathLen);
+    chdir(dirname(path));
+    chdir("../../");
+#   endif
+
     MainWindow::instance = new MainWindow();
     Configuration::instance->readConfig();
     return true;
