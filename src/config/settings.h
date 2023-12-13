9a9a50b180c513ed07986aee13f36045c057ff83
ryancog <170381220+ryancog@users.noreply.github.com>
2023-12-13 01:28:08 -0600

    Top Define Settings Parser In Progress

--- src/config/settings.h
+++ src/config/settings.h
@@ -13,23 +13,30 @@ public:
   Settings();
   static Settings* instance;
 
+  void linkDefines();
+  void setCustomDefineParsers();
+
   class ProffieDefine;
+  std::unordered_map<std::string, ProffieDefine*> defines;
 
 private:
 };
 
 class Settings::ProffieDefine {
 public:
-  ProffieDefine(std::string name, int32_t defaultValue, Misc::numEntry* element, bool loose);
-  ProffieDefine(std::string name, double defaultValue, Misc::numEntryDouble* element, bool loose);
-  ProffieDefine(std::string name, bool defaultState, wxCheckBox* element, bool loose);
-  ProffieDefine(std::string name, wxString defaultSelection, wxComboBox* element, bool loose);
+  static std::pair<std::string, ProffieDefine*> createProffieDefine(std::string name, int32_t defaultValue, Misc::numEntry* element, bool loose = false);
+  static std::pair<std::string, ProffieDefine*> createProffieDefine(std::string name, double defaultValue, Misc::numEntryDouble* element, bool loose = false);
+  static std::pair<std::string, ProffieDefine*> createProffieDefine(std::string name, bool defaultState, wxCheckBox* element, bool loose = false);
+  static std::pair<std::string, ProffieDefine*> createProffieDefine(std::string name, wxString defaultSelection, wxComboBox* element, bool loose = false);
 
-  inline void overrideParser(std::function<bool(std::string&)> newParser);
-  inline void* getElementReference();
+  static std::pair<std::string, std::string> parseKey(const std::string&);
 
+  inline void overrideParser(std::function<bool(const std::string&)> newParser);
+  void loadDefault();
 
 private:
+  ProffieDefine();
+
   enum class Type {
     STATE,
     NUMERIC,
@@ -46,8 +53,7 @@ private:
 
   Type type;
   std::string identifier;
-
   void* element;
 
-  std::function<bool(std::string&)> parseDefine;
+  std::function<bool(const std::string&)> parseDefine;
 };
