5c4e10f276e3f57a51925b29f0fd0ab5df30c291
ryancog <170381220+ryancog@users.noreply.github.com>
2025-03-11 04:18:24 -0500

    Remove unused defines. Reformat version defines

--- src/proffieconfig/core/defines.h
+++ src/proffieconfig/core/defines.h
@@ -1,10 +1,9 @@
+#pragma once
 // ProffieConfig, All-In-One GUI Proffieboard Configuration Utility
 // Copyright (C) 2025 Ryan Ogurek
 
-#pragma once
-
-#define PROFFIEOS_VERSION "7.14"
-#define ARDUINO_CLI_VERSION "0.34.2"
+#define PROFFIEOS_VERSION 7.15
+#define ARDUINO_CLI_VERSION 0.34.2
 
 #define COPYRIGHT_NOTICE \
   "ProffieConfig Copyright (C) 2023-2025 Ryan Ogurek\n" \
@@ -30,13 +29,6 @@
 #define UPDATEWINDOW(window) window->sizer->Layout(); window->Refresh();
 #define FULLUPDATEWINDOW(window) window->sizer->Layout(); window->SetSizerAndFit(window->sizer); window->Refresh();
 
-#define PR_DEFAULT "Default"
-#define PR_SA22C "SA22C"
-#define PR_FETT263 "Fett263"
-#define PR_SHTOK "Shtok"
-#define PR_BC "BC"
-#define PR_CAIWYN "Caiwyn"
-
 #include <wx/tooltip.h>
 #define TIP(object, msg) object->SetToolTip(new wxToolTip(msg));
 
@@ -47,30 +39,3 @@
 #define ARDUINO_PBPLUGIN_VERSION "3.6" // Make sure the compile output parsing doesn't break if we update this!
 
 #define SMALLBUTTONSIZE wxSize(30, 20)
-
-#if defined(__WINDOWS__)
-#define RESOURCES_PATH "resources\\"
-#define ARDUINO_PATH RESOURCES_PATH "arduino-cli\\arduino-cli.exe"
-#define PROFFIEOS_INO PROFFIEOS_PATH "\\ProffieOS.ino"
-#define CONFIG_DIR PROFFIEOS_PATH "\\config\\"
-#define PROPCONFIG_DIR RESOURCES_PATH "props\\"
-#define STYLEEDIT_PATH RESOURCES_PATH "StyleEditor\\style_editor.html"
-#elif defined(__WXGTK__)
-#define RESOURCES_PATH "resources/"
-#define ARDUINO_PATH RESOURCES_PATH "arduino-cli/arduino-cli"
-#define PROFFIEOS_INO PROFFIEOS_PATH "/ProffieOS.ino"
-#define CONFIG_DIR PROFFIEOS_PATH "/config/"
-#define PROPCONFIG_DIR RESOURCES_PATH "props/"
-#define STYLEEDIT_PATH RESOURCES_PATH "StyleEditor/style_editor.html"
-#elif defined(__WXOSX__)
-#define RESOURCES_PATH "../Resources/"
-#define ARDUINO_PATH RESOURCES_PATH "arduino-cli/arduino-cli"
-#define PROFFIEOS_INO PROFFIEOS_PATH "/ProffieOS.ino"
-#define CONFIG_DIR PROFFIEOS_PATH "/config/"
-#define PROPCONFIG_DIR RESOURCES_PATH "props/"
-#define DRIVER_INSTALL popen("", "r");
-#define STYLEEDIT_PATH RESOURCES_PATH "StyleEditor/style_editor.html"
-#endif
-
-#define STATEFILE_PATH RESOURCES_PATH ".state.pconf"
-#define PROFFIEOS_PATH RESOURCES_PATH "ProffieOS"
