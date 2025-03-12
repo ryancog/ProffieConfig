bc0e4309b6e822e4fd92d43d2a84dc8ef9f44efe
ryancog <170381220+ryancog@users.noreply.github.com>
2025-03-11 19:07:16 -0500

    Update copyright notice with reference to ProffieOS for good measure

--- src/proffieconfig/core/defines.h
+++ src/proffieconfig/core/defines.h
@@ -5,21 +5,27 @@
 #define PROFFIEOS_VERSION 7.15
 #define ARDUINO_CLI_VERSION 0.34.2
 
-#define COPYRIGHT_NOTICE \
-  "ProffieConfig Copyright (C) 2023-2025 Ryan Ogurek\n" \
-  "\n" \
-  "This program is free software: you can redistribute it and/or modify " \
-  "it under the terms of the GNU General Public License as published by " \
-  "the Free Software Foundation, either version 3 of the License, or " \
-  "(at your option) any later version.\n" \
-  "\n" \
-  "This program is distributed in the hope that it will be useful, " \
-  "but WITHOUT ANY WARRANTY; without even the implied warranty of " \
-  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the " \
-  "GNU General Public License for more details.\n" \
-  "\n" \
-  "You should have received a copy of the GNU General Public License " \
-  "along with this program. If not, see https://www.gnu.org/licenses/."
+static constexpr auto COPYRIGHT_NOTICE{
+    "ProffieConfig Copyright (C) 2023-2025 Ryan Ogurek\n"
+    "\n"
+    "This program is free software: you can redistribute it and/or modify "
+    "it under the terms of the GNU General Public License as published by "
+    "the Free Software Foundation, either version 3 of the License, or "
+    "(at your option) any later version.\n"
+    "\n"
+    "This program is distributed in the hope that it will be useful, "
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+    "GNU General Public License for more details.\n"
+    "\n"
+    "You should have received a copy of the GNU General Public License "
+    "along with this program. If not, see https://www.gnu.org/licenses/.\n"
+    "\n"
+    "Additionally, this program uses code from ProffieOS "
+    "(copyright Fredrik Hubinette et al.) in accordance with the GPLv3 "
+    "license of the ProffieOS software. It further references other tools "
+    "related to ProffieOS, created by Fredrik Hubinette."
+};
 
 #define FIRSTITEMFLAGS wxSizerFlags(0).Border(wxALL, 5)
 #define MENUITEMFLAGS  wxSizerFlags(0).Border(wxLEFT | wxBOTTOM | wxRIGHT, 5)
