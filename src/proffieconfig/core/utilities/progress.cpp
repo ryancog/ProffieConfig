97b0f328b5bb73136f1a47e0d5457d15e83f19cf
ryancog <170381220+ryancog@users.noreply.github.com>
2025-03-17 15:30:36 -0500

    Progress: Use message on Pulse; Try to cleanup wxGauge taskbar/dock status

--- src/proffieconfig/core/utilities/progress.cpp
+++ src/proffieconfig/core/utilities/progress.cpp
@@ -1,4 +1,5 @@
 #include "progress.h"
+#include "wx/utils.h"
 // ProffieConfig, All-In-One GUI Proffieboard Configuration Utility
 // Copyright (C) 2025 Ryan Ogurek
 
@@ -7,24 +8,26 @@
 wxEventTypeTag<wxCommandEvent> Progress::EVT_UPDATE(wxNewEventType());
 
 void Progress::emitEvent(int8_t progress, wxString message) {
-  ProgressEvent* event = new ProgressEvent(EVT_UPDATE, wxID_ANY);
-  event->progress = progress;
-  event->message = message;
-  event->progDialog = this;
-  wxQueueEvent(GetParent()->GetEventHandler(), event);
+    ProgressEvent* event = new ProgressEvent(EVT_UPDATE, wxID_ANY);
+    event->progress = progress;
+    event->message = message;
+    event->progDialog = this;
+    wxQueueEvent(GetParent()->GetEventHandler(), event);
 }
 
 void Progress::handleEvent(ProgressEvent* event) {
-  if (event->progress > event->progDialog->GetValue() || (event->progDialog->lastWasPulse && event->progress != -1)) {
-    event->progDialog->lastWasPulse = false;
-    event->progDialog->Update(event->progress, event->message);
-  }
-  else if (event->progress == -1) {
-    event->progDialog->lastWasPulse = true;
-    event->progDialog->Pulse();
-  }
+    if (event->progress > event->progDialog->GetValue() || (event->progDialog->lastWasPulse && event->progress != -1)) {
+        event->progDialog->lastWasPulse = false;
+        event->progDialog->Update(event->progress, event->message);
+    }
+    else if (event->progress == -1) {
+        event->progDialog->lastWasPulse = true;
+        event->progDialog->Pulse(event->message);
+    }
 
-  if (event->progress == 100) {
-    event->progDialog->Close(true);
-  }
+    if (event->progress == 100) {
+        event->progDialog->Update(0, "");
+        wxYield();
+        event->progDialog->Close(true);
+    }
 }
