4f8b7996a374ad095fab689b40a22577635dccbd
ryancog <170381220+ryancog@users.noreply.github.com>
2025-07-16 19:07:57 -0400

    Update mainmenu to use new control scheme

--- src/proffieconfig/mainmenu/dialogs/addconfig.cpp
+++ src/proffieconfig/mainmenu/dialogs/addconfig.cpp
@@ -1,4 +1,6 @@
 #include "addconfig.h"
+#include "ui/controls.h"
+#include "wx/osx/stattext.h"
 // ProffieConfig, All-In-One GUI Proffieboard Configuration Utility
 // Copyright (C) 2025 Ryan Ogurek
 
@@ -23,82 +25,134 @@ AddConfig::AddConfig(MainMenu *parent) :
 }
 
 void AddConfig::bindEvents() {
-    Bind(wxEVT_BUTTON, [&](wxCommandEvent&) {
-        if (importExisting->GetValue()) {
-            existingPath = chooseConfig->GetFileName().GetAbsolutePath().ToStdString();
-        } 
-
-        configName = configNameEntry->entry()->GetValue().ToStdString();
-        EndModal(wxID_OK);
-    }, wxID_OK);
+    // Bind(wxEVT_BUTTON, [&](wxCommandEvent&) { EndModal(wxID_OK); }, wxID_OK);
     // We make sure to set itself to true that way it can't be deselected
-    Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent&) { importExisting->SetValue(true); createNew->SetValue(false); update(); }, ID_ImportExisting);
-    Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent&) { createNew->SetValue(true); importExisting->SetValue(false); update(); }, ID_CreateNew);
-    Bind(wxEVT_TEXT, [&](wxCommandEvent&) { update(); });
-    Bind(wxEVT_FILEPICKER_CHANGED, [&](wxCommandEvent&) {
-        if (chooseConfig->GetFileName().FileExists()) {
-            configNameEntry->entry()->SetValue(chooseConfig->GetFileName().GetName());
-        }
-        update();
-    });
+    Bind(wxEVT_TOGGLEBUTTON, [this](wxCommandEvent&) { 
+        static_cast<wxToggleButton *>(FindWindowById(ID_ImportExisting))->SetValue(true);
+        static_cast<wxToggleButton *>(FindWindowById(ID_CreateNew))->SetValue(false);
+        update(); 
+    }, ID_ImportExisting);
+    Bind(wxEVT_TOGGLEBUTTON, [this](wxCommandEvent&) { 
+        static_cast<wxToggleButton *>(FindWindowById(ID_CreateNew))->SetValue(true);
+        static_cast<wxToggleButton *>(FindWindowById(ID_ImportExisting))->SetValue(false);
+        importPath = filepath{};
+        update(); 
+    }, ID_CreateNew);
+    configName.onValueUpdate = [this]() { update(); };
+    importPath.onValueUpdate = [this]() {
+        configName = static_cast<filepath>(importPath).stem();
+    };
 }
 
 void AddConfig::createUI() {
-  auto *sizer{new wxBoxSizer(wxVERTICAL)};
-  sizer->SetMinSize(wxSize(400, -1));
+    auto *sizer{new wxBoxSizer(wxVERTICAL)};
+    sizer->SetMinSize(wxSize(400, -1));
 
-  auto *modeSelection{new wxBoxSizer(wxHORIZONTAL)};
-  createNew = new wxToggleButton(this, ID_CreateNew, _("Create New Config"));
-  createNew->SetValue(true);
-  importExisting = new wxToggleButton(this, ID_ImportExisting, _("Import Existing Config"));
-  modeSelection->Add(createNew, wxSizerFlags(0).Border(wxLEFT | wxTOP | wxBOTTOM, 10));
-  modeSelection->Add(importExisting, wxSizerFlags(0).Border(wxRIGHT | wxTOP | wxBOTTOM, 10));
+    auto *addModeSizer{new wxBoxSizer(wxHORIZONTAL)};
+    auto *createNewToggle{
+        new wxToggleButton(this, ID_CreateNew, _("Create New Config"))
+    };
+    createNewToggle->SetValue(true);
+    auto *importExistingToggle{
+        new wxToggleButton(this, ID_ImportExisting, _("Import Existing Config"))
+    };
+    addModeSizer->Add(
+        createNewToggle,
+        wxSizerFlags(0).Border(wxLEFT | wxTOP | wxBOTTOM, 10)
+    );
+    addModeSizer->Add(
+        importExistingToggle,
+        wxSizerFlags(0).Border(wxRIGHT | wxTOP | wxBOTTOM, 10)
+    );
 
-  mChooseConfigText = new wxStaticText(this, wxID_ANY, _("Configuration to Import"));
-  chooseConfig = new wxFilePickerCtrl(this, ID_ChooseConfig, wxEmptyString, _("Choose Configuration File to Import"), "ProffieOS Configuration (*.h)|*.h");
+    auto *importPicker{new PCUI::FilePicker(
+        this,
+        importPath,
+        wxFLP_FILE_MUST_EXIST | wxFLP_OPEN | wxFLP_USE_TEXTCTRL,
+        _("Configuration to Import"),
+        "ProffieOS Configuration (*.h)|*.h",
+        _("Choose Configuration File to Import")
+    )};
+    auto *nameEntry{new PCUI::Text(
+        this,
+        configName,
+        0,
+        _("Configuration Name")
+    )};
 
-  configNameEntry = new PCUI::Text(this, ID_ConfigName, {}, 0, _("Configuration Name"));
+    mInvalidNameWarning = new wxStaticText(
+        this,
+        wxID_ANY,
+        _("Please enter a valid name")
+    );
+    mDuplicateWarning = new wxStaticText(
+        this,
+        wxID_ANY,
+        _("Configuration with same name already exists")
+    );
+    mFileSelectionWarning = new wxStaticText(
+        this,
+        wxID_ANY,
+        _("Please choose a configuration file to import")
+    );
 
-  mInvalidNameWarning = new wxStaticText(this, wxID_ANY, _("Please enter a valid name"));
-  mDuplicateWarning = new wxStaticText(this, wxID_ANY, _("Configuration with same name already exists"));
-  mFileSelectionWarning = new wxStaticText(this, wxID_ANY, _("Please choose a configuration file to import"));
+    sizer->Add(addModeSizer, wxSizerFlags(0).Center());
+    sizer->Add(
+        importPicker,
+        wxSizerFlags(0).Expand().Border(wxALL, 10)
+    );
+    sizer->Add(
+        nameEntry,
+        wxSizerFlags(0)
+            .Expand().Border(wxBOTTOM | wxLEFT | wxRIGHT, 10)
+    );
+    sizer->Add(
+        mInvalidNameWarning,
+        wxSizerFlags(0).Right().Border(wxRIGHT, 10)
+    );
+    sizer->Add(
+        mDuplicateWarning,
+        wxSizerFlags(0).Right().Border(wxRIGHT, 10)
+    );
+    sizer->Add(
+        mFileSelectionWarning,
+        wxSizerFlags(0).Right().Border(wxRIGHT, 10)
+    );
+    sizer->AddStretchSpacer(1);
+    sizer->Add(
+        CreateStdDialogButtonSizer(wxOK | wxCANCEL),
+        wxSizerFlags(0).Border(wxALL, 10).Expand()
+    );
 
-  sizer->Add(modeSelection, wxSizerFlags(0).Center());
-  sizer->Add(mChooseConfigText, wxSizerFlags(0).Border(wxTOP | wxLEFT | wxRIGHT, 10).DoubleBorder(wxLEFT));
-  sizer->Add(chooseConfig, wxSizerFlags(0).Expand().Border(wxBOTTOM | wxLEFT | wxRIGHT, 10));
-  sizer->Add(configNameEntry, wxSizerFlags(0).Expand().Border(wxBOTTOM | wxLEFT | wxRIGHT, 10));
-  sizer->Add(mInvalidNameWarning, wxSizerFlags(0).Right().Border(wxRIGHT, 10));
-  sizer->Add(mDuplicateWarning, wxSizerFlags(0).Right().Border(wxRIGHT, 10));
-  sizer->Add(mFileSelectionWarning, wxSizerFlags(0).Right().Border(wxRIGHT, 10));
-  sizer->AddStretchSpacer(1);
-  sizer->Add(CreateStdDialogButtonSizer(wxOK | wxCANCEL), wxSizerFlags(0).Border(wxALL, 10).Expand());
-
-  SetSizerAndFit(sizer);
-  update();
+    SetSizerAndFit(sizer);
+    update();
 }
 
 void AddConfig::update() {
   auto duplicateConfigName = [&]() { 
+      // TODO: Fix this???
       // for (const auto& config : getConfigFileNames()) {
       //     if (configName->entry()->GetValue() == config) return true;
       // }
       return false;
   }();
 
-  auto configNameEmpty = configNameEntry->entry()->GetValue().empty();
-  auto configNameInvalidCharacters = configNameEntry->entry()->GetValue().find_first_of(".\\,/!#$%^&*|?<>\"'") != string::npos;
-  auto validConfigName = !configNameEmpty && !duplicateConfigName && !configNameInvalidCharacters;
-  auto importingConfig = importExisting->GetValue();
-  auto originFileSelected = chooseConfig->GetFileName().FileExists();
+  const auto& configNameText{static_cast<string>(configName)};
+  bool configNameEmpty{configNameText.empty()};
+  bool configNameInvalidCharacters{
+    configNameText.find_first_of(".\\,/!#$%^&*|?<>\"'") != string::npos
+  };
+  bool validConfigName{not configNameEmpty and not duplicateConfigName and not configNameInvalidCharacters};
+  bool importingConfig{static_cast<wxToggleButton *>(FindWindowById(ID_ImportExisting))->GetValue()};
+  bool originFileSelected{not static_cast<filepath>(importPath).empty()};
 
   mDuplicateWarning->Show(duplicateConfigName);
-  mInvalidNameWarning->Show(!validConfigName);
-  mFileSelectionWarning->Show(importingConfig && !originFileSelected);
+  mInvalidNameWarning->Show(not validConfigName);
+  mFileSelectionWarning->Show(importingConfig and not originFileSelected);
 
-  FindWindowById(wxID_OK)->Enable(validConfigName && (importingConfig ? originFileSelected : true));
+  FindWindowById(wxID_OK)->Enable(validConfigName and (originFileSelected or not importingConfig));
 
-  mChooseConfigText->Show(importingConfig);
-  chooseConfig->Show(importingConfig);
+  importPath.show(importingConfig);
 
   Layout(); // Although linux and windows seem to work without this, macOS requires it.
   Fit();
