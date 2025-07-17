facb1afccf04a7955644e5a6b1dae1f956b2aa84
ryancog <170381220+ryancog@users.noreply.github.com>
2025-07-17 12:17:57 -0400

    Config::Settings use PCUI (no handlers)

--- src/components/config/settings/settings.h
+++ src/components/config/settings/settings.h
@@ -1,7 +1,7 @@
 #pragma once
 /*
  * ProffieConfig, All-In-One Proffieboard Management Utility
- * Copyright (C) 2024 Ryan Ogurek
+ * Copyright (C) 2024-2025 Ryan Ogurek
  *
  * components/settings/settings.h
  *
@@ -19,11 +19,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <utils/types.h>
+#include "ui/controls.h"
+#include "utils/types.h"
 
 namespace Config {
 
 struct Settings {
+    /**
+     * Set up defaults and update handlers
+     */
+    Settings();
+
     constexpr static cstring NUM_BLADES_STR{"NUM_BLADES"};
     constexpr static cstring NUM_BUTTONS_STR{"NUM_BUTTONS"};
     constexpr static cstring EN_AUDIO_STR{"ENABLE_AUDIO"};
@@ -33,17 +39,42 @@ struct Settings {
     constexpr static cstring SHARED_POWER_PINS_STR{"SHARED_POWER_PINS"};
     constexpr static cstring KEEP_SAVEFILES_STR{"KEEP_SAVEFILES_WHEN_PROGRAMMING"};
 
-    constexpr static cstring SAVE_STATE_STR{"SAVE_STATE"};
-    constexpr static cstring ALL_EDIT_OPTS_STR{"ENABLE_ALL_EDIT_OPTIONS"};
-
+    // PCUI::ChoiceData rfidSerial;
     constexpr static cstring RFID_SERIAL_STR{"RFID_SERIAL"};
+
+    PCUI::NumericData volume;
+    constexpr static cstring VOLUME_STR{"VOLUME"};
+    PCUI::ToggleData enableBootVolume;
+    PCUI::NumericData bootVolume;
+    constexpr static cstring BOOT_VOLUME_STR{"BOOT_VOLUME"};
+    PCUI::DecimalData clashThreshold;
+    constexpr static cstring CLASH_THRESHOLD_STR{"CLASH_THRESHOLD_G"};
+
+    // In minutes
+    PCUI::DecimalData pliOffTime;
+    constexpr static cstring PLI_OFF_STR{"PLI_OFF_TIME"};
+    PCUI::DecimalData idleOffTime;
+    constexpr static cstring IDLE_OFF_STR{"IDLE_OFF_TIME"};
+    PCUI::DecimalData motionOffTime;
+    constexpr static cstring MOTION_OFF_STR{"MOTION_TIMEOUT"};
+
+    PCUI::ToggleData disableColorChange;
+    constexpr static cstring DISABLE_COLOR_CHANGE_STR{"DISABLE_COLOR_CHANGE"};
+    PCUI::ToggleData disableBasicParserStyles;
+    constexpr static cstring DISABLE_BASIC_PARSERS_STR{"DISABLE_BASIC_PARSER_STYLES"};
+    PCUI::ToggleData disableDiagnosticCommands;
+    constexpr static cstring DISABLE_DIAG_COMMANDS_STR{"DISABLE_DIAGNOSTIC_COMMANDS"};
+    PCUI::ToggleData enableDeveloperCommands;
+    constexpr static cstring ENABLE_DEV_COMMANDS_STR{"ENABLE_DEVELOPER_COMMANDS"};
 
     // SAVE_STATE Sets:
     // - saveVolume
     // - savePreset
     // - saveColorChange
     // - saveBladeDimming (only if dynamicBladeDimming is set)
-    
+    PCUI::ToggleData saveState;
+    constexpr static cstring SAVE_STATE_STR{"SAVE_STATE"};
+
     // ENABLE_ALL_EDIT_OPTIONS Sets:
     // - dynamicBladeLength
     // - dynamicBladeDimming
@@ -52,46 +83,25 @@ struct Settings {
     // - saveColorChange
     // - saveBladeDimming
     // - saveClashThreshold
+    PCUI::ToggleData enableAllEditOptions;
+    constexpr static cstring ENABLE_ALL_EDIT_OPTIONS_STR{"ENABLE_ALL_EDIT_OPTIONS"};
 
-    int32 volume{1000};
-    constexpr static cstring VOLUME_STR{"VOLUME"};
-    int32 bootVolume{-1};
-    constexpr static cstring BOOT_VOLUME_STR{"BOOT_VOLUME"};
-    float32 clashThreshold{3};
-    constexpr static cstring CLASH_THRESHOLD_STR{"CLASH_THRESHOLD_G"};
-
-    // In seconds
-    int32 pliOffTime{10};
-    constexpr static cstring PLI_OFF_STR{"PLI_OFF_TIME"};
-    int32 idleOffTime{10 * 60};
-    constexpr static cstring IDLE_OFF_STR{"IDLE_OFF_TIME"};
-    int32 motionOffTime{15 * 60};
-    constexpr static cstring MOTION_OFF_STR{"MOTION_TIMEOUT"};
-
-    bool disableColorChange{false};
-    constexpr static cstring DISABLE_COLOR_CHANGE_STR{"DISABLE_COLOR_CHANGE"};
-    bool disableBasicParserStyles{false};
-    constexpr static cstring DISABLE_BASIC_PARSERS_STR{"DISABLE_BASIC_PARSER_STYLES"};
-    bool disableDiagnosticCommands{false};
-    constexpr static cstring DISABLE_DIAG_COMMANDS_STR{"DISABLE_DIAGNOSTIC_COMMANDS"};
-    bool enableDeveloperCommands{false};
-    constexpr static cstring ENABLE_DEV_COMMANDS_STR{"ENABLE_DEVELOPER_COMMANDS"};
-
-    bool saveColorChange{false};
+    PCUI::ToggleData saveColorChange;
     constexpr static cstring SAVE_COLOR_STR{"SAVE_COLOR_CHANGE"};
-    bool saveVolume{false};
+    PCUI::ToggleData saveVolume;
     constexpr static cstring SAVE_VOLUME_STR{"SAVE_VOLUME"};
-    bool savePreset{false};
+    PCUI::ToggleData savePreset;
     constexpr static cstring SAVE_PRESET_STR{"SAVE_PRESET"};
 
-    enum class Orientation {
+    enum Orientation {
         FETS_TOWARDS_BLADE,
         USB_TOWARDS_BLADE,
         USB_CCW_FROM_BLADE,
         USB_CW_FROM_BLADE,
         TOP_TOWARDS_BLADE,
         BOTTOM_TOWARDS_BLADE,
-    } orientation{Orientation::FETS_TOWARDS_BLADE};
+    };
+    PCUI::ChoiceData orientation;
     constexpr static cstring ORIENTATION_STR{"ORIENTATION"};
     constexpr static cstring ORIENT_FETS_TOWARDS_BLADE_STR{"ORIENTATION_FETS_TOWARDS_BLADE"};
     constexpr static cstring ORIENT_USB_TOWARDS_BLADE_STR{"ORIENTATION_USB_TOWARDS_BLADE"};
@@ -101,14 +111,14 @@ struct Settings {
     constexpr static cstring ORIENT_BOTTOM_TOWARDS_BLADE_STR{"ORIENTATION_BOTTOM_TOWARDS_BLADE"};
 
     struct OrientRotation {
-        int32 x{0};
-        int32 y{0};
-        int32 z{0};
-    } orientationRotation{};
+        PCUI::NumericData x;
+        PCUI::NumericData y;
+        PCUI::NumericData z;
+    } orientationRotation;
     constexpr static cstring ORIENTATION_ROTATION_STR{"ORIENTATION_ROTATION"};
 
     // For debugging touch buttons:
-    bool speakTouchValues{false};
+    PCUI::ToggleData speakTouchValues;
     constexpr static cstring SPEAK_TOUCH_VALUES_STR{"SPEAK_TOUCH_VALUES"};
     
     // Make these be enabled by adding such a device to the
@@ -119,39 +129,39 @@ struct Settings {
     int32 lineOutVolume{2000}; // This can be set to `dynamic_mixer.get_volume()` to follow master
     */
 
-    bool dynamicBladeDimming{false};
-    constexpr static cstring DYN_BLADE_DIM_STR{"DYNAMIC_BLADE_DIMMING"};
-    bool dynamicBladeLength{false};
-    constexpr static cstring DYN_BLADE_LENGTH_STR{"DYNAMIC_BLADE_LENGTH"};
-    bool dynamicClashThreshold{false};
-    constexpr static cstring DYN_CLASH_THRESHOLD_STR{"DYNAMIC_CLASH_THRESHOLD"};
+    PCUI::ToggleData dynamicBladeDimming;
+    constexpr static cstring DYNAMIC_BLADE_DIMMING_STR{"DYNAMIC_BLADE_DIMMING"};
+    PCUI::ToggleData dynamicBladeLength;
+    constexpr static cstring DYNAMIC_BLADE_LENGTH_STR{"DYNAMIC_BLADE_LENGTH"};
+    PCUI::ToggleData dynamicClashThreshold;
+    constexpr static cstring DYNAMIC_CLASH_THRESHOLD_STR{"DYNAMIC_CLASH_THRESHOLD"};
 
     // only should be settable if dynamicBladeDimming is true
-    bool saveBladeDimming{false};
+    PCUI::ToggleData saveBladeDimming;
     constexpr static cstring SAVE_BLADE_DIM_STR{"SAVE_BLADE_DIMMING"};
-    bool saveClashThreshold{false};
+    PCUI::ToggleData saveClashThreshold;
     constexpr static cstring SAVE_CLASH_THRESHOLD_STR{"SAVE_CLASH_THRESHOLD"};
 
     // Does not affect I2C or S/PDIF
     // Cutoff in Hz
-    int32 filterCutoff{100};
+    PCUI::NumericData filterCutoff;
     constexpr static cstring FILTER_CUTOFF_STR{"FILTER_CUTOFF_FREQUENCY"};
-    int32 filterOrder{8};
+    PCUI::NumericData filterOrder;
     constexpr static cstring FILTER_ORDER_STR{"FILTER_ORDER"};
 
     // Useful range is 1~50
-    int8 audioClashSuppressionLevel{10};
+    PCUI::NumericData audioClashSuppressionLevel;
     constexpr static cstring AUDIO_CLASH_SUPPRESSION_STR{"AUDIO_CLASH_SUPPRESSION_LEVEL"};
-    bool dontUseGyroForClash{false};
+    PCUI::ToggleData dontUseGyroForClash;
     constexpr static cstring DONT_USE_GYRO_FOR_CLASH_STR{"PROFFIEOS_DONT_USE_GYRO_FOR_CLASH"};
 
-    bool noRepeatRandom{false};
+    PCUI::ToggleData noRepeatRandom;
     constexpr static cstring NO_REPEAT_RANDOM_STR{"NO_REPEAT_RANDOM"};
-    bool femaleTalkie{false};
+    PCUI::ToggleData femaleTalkie;
     constexpr static cstring FEMALE_TALKIE_STR{"FEMALE_TALKIE_VOICE"};
-    bool disableTalkie{false};
+    PCUI::ToggleData disableTalkie;
     constexpr static cstring DISABLE_TALKIE_STR{"DISABLE_TALKIE"};
-    bool killOldPlayers{false};
+    PCUI::ToggleData killOldPlayers;
     constexpr static cstring KILL_OLD_PLAYERS_STR{"KILL_OLD_PLAYERS"};
 
     // POV Data?
