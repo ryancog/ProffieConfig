c2e163d4673d27236dc3c209fa6ed09a036b2893
ryancog <170381220+ryancog@users.noreply.github.com>
2025-07-22 10:29:34 -0400

    Core: Proper Message and Progress Events
    Probably going to get ripped out in near future anyways but whatever

--- src/proffieconfig/core/utilities/misc.cpp
+++ src/proffieconfig/core/utilities/misc.cpp
@@ -2,5 +2,5 @@
 // ProffieConfig, All-In-One GUI Proffieboard Configuration Utility
 // Copyright (C) 2025 Ryan Ogurek
 
-const wxEventTypeTag<wxCommandEvent> Misc::EVT_MSGBOX(wxNewEventType());
+const wxEventTypeTag<Misc::MessageBoxEvent> Misc::EVT_MSGBOX(wxNewEventType());
 
