f4ac2aaf3d2e42f053a52f534e51089044c4dbd5
ryancog <170381220+ryancog@users.noreply.github.com>
2025-08-10 18:53:41 -0400

    Cleanup presetspage and add/update tooltips

--- src/proffieconfig/editor/pages/presetspage.h
+++ src/proffieconfig/editor/pages/presetspage.h
@@ -1,15 +1,25 @@
 #pragma once
-// ProffieConfig, All-In-One GUI Proffieboard Configuration Utility
-// Copyright (C) 2023-2025 Ryan Ogurek
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2023-2025 Ryan Ogurek
+ *
+ * proffieconfig/editor/pages/presetspage.h
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <wx/textctrl.h>
 #include <wx/sizer.h>
-#include <wx/wrapsizer.h>
-#include <wx/checkbox.h>
-#include <wx/spinctrl.h>
-#include <wx/combobox.h>
-#include <wx/listbox.h>
-#include <wx/button.h>
 
 #include "../editorwindow.h"
 
