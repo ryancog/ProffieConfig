86cc4091188c75523dc121d94cc581aba890085f
ryancog <170381220+ryancog@users.noreply.github.com>
2025-08-17 14:43:37 -0400

    Tweaks to propspage; pending layout/ui building

--- src/proffieconfig/editor/pages/propspage.cpp
+++ src/proffieconfig/editor/pages/propspage.cpp
@@ -1,6 +1,23 @@
 #include "propspage.h"
-// ProffieConfig, All-In-One GUI Proffieboard Configuration Utility
-// Copyright (C) 2025 Ryan Ogurek
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2023-2025 Ryan Ogurek
+ *
+ * proffieconfig/editor/pages/propspage.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <wx/button.h>
 #include <wx/event.h>
@@ -9,14 +26,13 @@
 #include <wx/sizer.h>
 #include <wx/tooltip.h>
 
-#include "../../core/defines.h"
 #include "../editorwindow.h"
 #include "../dialogs/propbuttons.h"
 
 PropsPage::PropsPage(EditorWindow *parent) : 
     wxPanel(parent),
     mParent{parent} {
-    Notifier::create(this, mNotifyData);
+    Notifier::create(this, mParent->getOpenConfig().propNotifyData);
     auto& config{mParent->getOpenConfig()};
 
     auto *sizer{new wxBoxSizer(wxVERTICAL)};
@@ -54,7 +70,6 @@ PropsPage::PropsPage(EditorWindow *parent) :
     sizer->Add(topSizer);
     sizer->Add(mPropsWindow, wxSizerFlags(1).Expand());
 
-    loadProps();
     bindEvents();
     initializeNotifier();
     SetSizerAndFit(sizer);
@@ -88,11 +103,24 @@ void PropsPage::bindEvents() {
 }
 
 void PropsPage::handleNotification(uint32 id) {
-    if (id == ID_REBOUND or id == ID_PropSelection) {
-        auto& config{mParent->getOpenConfig()};
-        FindWindow(ID_Buttons)->Enable(config.propSelection != -1);
-        FindWindow(ID_PropInfo)->Enable(config.propSelection != -1);
+    if (id == ID_REBOUND or id == Config::Config::ID_PROPSELECTION) {
+        showSelectedProp();
     }
+    if (id == ID_REBOUND or id == Config::Config::ID_PROPUPDATE) {
+        loadProps();
+    }
+}
+
+void PropsPage::showSelectedProp() {
+    auto& config{mParent->getOpenConfig()};
+    bool hasValidSelection{config.propSelection != -1 and config.propSelection < config.props().size()};
+    FindWindow(ID_Buttons)->Enable(hasValidSelection);
+    FindWindow(ID_PropInfo)->Enable(hasValidSelection);
+
+    // Hide all
+    if (not hasValidSelection) return;
+
+    // Show
 }
 
 // void PropsPage::update() {
