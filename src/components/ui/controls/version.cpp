ab39c81b759d464dd6e40e16b107f44b1035cda4
ryancog <170381220+ryancog@users.noreply.github.com>
2025-08-17 23:34:35 -0400

    PCUI::Version Fix some logic

--- src/components/ui/controls/version.cpp
+++ src/components/ui/controls/version.cpp
@@ -25,12 +25,12 @@ PCUI::VersionData::VersionData() {
     mMajor.setUpdateHandler([this](uint32 id) {
         if (id != mMajor.ID_VALUE) return;
         
+
         auto rawValue{static_cast<string>(mMajor)};
+        bool jump{not rawValue.empty() and rawValue.back() == '.'};
         auto insertionPoint{mMajor.getInsertionPoint()};
         uint32 numTrimmed{};
         Utils::trimForNumeric(rawValue, &numTrimmed, insertionPoint);
-
-        if (numTrimmed == 0) return;
 
         if (mMajor.empty()) {
             mMajor = "0";
@@ -38,56 +38,94 @@ PCUI::VersionData::VersionData() {
             return;
         }
 
-        const auto clampedValue{std::clamp(
-            std::stoi(rawValue),
-            0,
-            Utils::Version::NULL_REV - 1
-        )};
-        mMajor = std::move(std::to_string(clampedValue));
-        mMajor.setInsertionPoint(insertionPoint - numTrimmed);
+        string newValue;
+        if (not rawValue.empty()) {
+            const auto clampedValue{std::clamp(
+                std::stoi(rawValue),
+                0,
+                Utils::Version::NULL_REV - 1
+            )};
+            newValue = std::to_string(clampedValue);
+        }
+
+        if (newValue != static_cast<string>(mMajor)) {
+            mMajor = std::move(newValue);
+            mMajor.setInsertionPoint(insertionPoint - numTrimmed);
+        }
+
+        if (jump) {
+            mMinor.setInsertionPointEnd();
+            mMinor.setFocus();
+        }
+        notify(ID_VALUE);
     });
     mMinor.setUpdateHandler([this](uint32 id) {
         if (id != mMinor.ID_VALUE) return;
 
         auto rawValue{static_cast<string>(mMinor)};
+        bool jump{not rawValue.empty() and rawValue.back() == '.'};
         auto insertionPoint{mMinor.getInsertionPoint()};
         uint32 numTrimmed{};
         Utils::trimForNumeric(rawValue, &numTrimmed, insertionPoint);
 
-        if (numTrimmed == 0) return;
+        string newValue;
+        if (not rawValue.empty()) {
+            const auto clampedValue{std::clamp(
+                std::stoi(rawValue),
+                0,
+                Utils::Version::NULL_REV - 1
+            )};
+            newValue = std::to_string(clampedValue);
+        }
 
-        const auto clampedValue{std::clamp(
-            std::stoi(rawValue),
-            0,
-            Utils::Version::NULL_REV - 1
-        )};
-        mMinor = std::move(std::to_string(clampedValue));
-        mMinor.setInsertionPoint(insertionPoint - numTrimmed);
+        if (newValue != static_cast<string>(mMinor)) {
+            mMinor = std::move(newValue);
+            mMinor.setInsertionPoint(insertionPoint - numTrimmed);
+        }
+
+        if (jump) {
+            mBugfix.setInsertionPointEnd();
+            mBugfix.setFocus();
+        }
+        notify(ID_VALUE);
     });
     mBugfix.setUpdateHandler([this](uint32 id) {
         if (id != mBugfix.ID_VALUE) return;
 
         auto rawValue{static_cast<string>(mBugfix)};
+        bool jump{not rawValue.empty() and rawValue.back() == '-'};
         auto insertionPoint{mBugfix.getInsertionPoint()};
         uint32 numTrimmed{};
         Utils::trimForNumeric(rawValue, &numTrimmed, insertionPoint);
-
-        if (numTrimmed == 0) return;
 
         if (mMinor.empty()) {
             mBugfix.clear();
             mMinor = std::move(rawValue);
             mMinor.setInsertionPoint(insertionPoint - numTrimmed);
+            mMinor.setFocus();
             return;
         }
 
-        const auto clampedValue{std::clamp(
-            std::stoi(rawValue),
-            0,
-            Utils::Version::NULL_REV - 1
-        )};
-        mBugfix = std::move(std::to_string(clampedValue));
-        mBugfix.setInsertionPoint(insertionPoint - numTrimmed);
+        string newValue;
+        if (not rawValue.empty()) {
+            const auto clampedValue{std::clamp(
+                std::stoi(rawValue),
+                0,
+                Utils::Version::NULL_REV - 1
+            )};
+            newValue = std::to_string(clampedValue);
+        }
+
+        if (newValue != static_cast<string>(mBugfix)) {
+            mBugfix = std::move(newValue);
+            mBugfix.setInsertionPoint(insertionPoint - numTrimmed);
+        }
+
+        if (jump) {
+            mTag.setInsertionPointEnd();
+            mTag.setFocus();
+        }
+        notify(ID_VALUE);
     });
     mTag.setUpdateHandler([this](uint32 id) {
         if (id != mTag.ID_VALUE) return;
@@ -101,7 +139,10 @@ PCUI::VersionData::VersionData() {
 
         mTag = std::move(rawValue);
         mTag.setInsertionPoint(insertionPoint - numTrimmed);
+        notify(ID_VALUE);
     });
+
+    mMajor = "0";
 }
 
 PCUI::VersionData::VersionData(const Utils::Version& version) : VersionData() {
@@ -127,8 +168,8 @@ void PCUI::VersionData::operator=(const Utils::Version& version) {
 void PCUI::VersionData::setValue(const Utils::Version& version) {
     std::scoped_lock scopeLock{getLock()};
     mMajor = std::to_string(version.major);
-    mMinor = std::to_string(version.minor);
-    mBugfix = std::to_string(version.bugfix);
+    mMinor = version.minor == Utils::Version::NULL_REV ? "" : std::to_string(version.minor);
+    mBugfix = version.bugfix == Utils::Version::NULL_REV ? "" : std::to_string(version.bugfix);
     mTag = string{version.tag};
     notify(ID_VALUE);
 }
@@ -157,13 +198,13 @@ void PCUI::Version::create(const wxString& label, wxOrientation orient) {
     if (dataProxy) {
         mMajor = new PCUI::Text(panel, dataProxy->mMajor);
         mMinor = new PCUI::Text(panel, dataProxy->mMinor);
-        mBugfix = new PCUI::Text(panel, dataProxy->mMinor);
-        mTag = new PCUI::Text(panel, dataProxy->mMinor);
+        mBugfix = new PCUI::Text(panel, dataProxy->mBugfix);
+        mTag = new PCUI::Text(panel, dataProxy->mTag);
     } else {
         mMajor = new PCUI::Text(panel, data()->mMajor);
         mMinor = new PCUI::Text(panel, data()->mMinor);
-        mBugfix = new PCUI::Text(panel, data()->mMinor);
-        mTag = new PCUI::Text(panel, data()->mMinor);
+        mBugfix = new PCUI::Text(panel, data()->mBugfix);
+        mTag = new PCUI::Text(panel, data()->mTag);
     }
 
     const auto textWidth{GetTextExtent("255").GetWidth() + 5};
@@ -184,7 +225,10 @@ void PCUI::Version::create(const wxString& label, wxOrientation orient) {
         wxSizerFlags().Bottom()
     );
     panelSizer->Add(mBugfix);
-    panelSizer->Add(new wxStaticText(panel, wxID_ANY, "-"));
+    panelSizer->Add(
+        new wxStaticText(panel, wxID_ANY, "-"),
+        wxSizerFlags().Center()
+    );
     panelSizer->Add(mTag, 1);
     panel->SetSizerAndFit(panelSizer);
 
