26c34e0ad5c6edbd35a2ba8deb006f56756aca22
ryancog <170381220+ryancog@users.noreply.github.com>
2025-08-22 16:18:07 -0400

    Add utils limits

---
+++ src/components/utils/limits.h
@@ -0,0 +1,32 @@
+#pragma once
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2025 Ryan Ogurek
+ *
+ * components/utils/limits.h
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <limits>
+
+namespace Utils {
+
+template<typename T>
+constexpr T MAX{std::numeric_limits<T>::max()};
+
+template<typename T>
+constexpr T MIN{std::numeric_limits<T>::min()};
+
+} // namespace Utils
