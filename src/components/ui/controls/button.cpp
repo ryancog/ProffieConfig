a47f24fe3074074a2204f3e2f08a0357bcd554d2
ryancog <170381220+ryancog@users.noreply.github.com>
2026-01-27 22:11:34 -0500

    PCUI: Button const ref dynamic color

--- src/components/ui/controls/button.cpp
+++ src/components/ui/controls/button.cpp
@@ -1,7 +1,7 @@
 #include "button.h"
 /*
  * ProffieConfig, All-In-One Proffieboard Management Utility
- * Copyright (C) 2025 Ryan Ogurek
+ * Copyright (C) 2025-2026 Ryan Ogurek
  *
  * components/ui/controls/button.cpp
  *
@@ -19,8 +19,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace PCUI {
-
 PCUI::Button::Button(
     wxWindow *parent,
     wxWindowID id,
@@ -29,7 +27,7 @@ PCUI::Button::Button(
     int64 style,
     const string& bmp,
     const wxSize& bmpSize,
-    Image::DynamicColor bmpColor
+    const Image::DynamicColor& bmpColor
 ) : wxButton(parent, id, label, wxDefaultPosition, size, style) {
     if (bmp.empty()) return;
 
@@ -43,6 +41,4 @@ PCUI::Button::Button(
     handler(evt);
     Bind(wxEVT_SYS_COLOUR_CHANGED, handler);
 }
-
-} // namespace PCUI
 
