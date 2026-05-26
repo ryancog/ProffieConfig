f89e9079e04361cae6de215829edd37d93897620
ryancog <170381220+ryancog@users.noreply.github.com>
2026-05-26 17:48:02 -0400

    [UI] Add Notebook

---
+++ src/components/ui/layout/notebook.cpp
@@ -0,0 +1,70 @@
+#include "notebook.hpp"
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2026 Ryan Ogurek
+ *
+ * components/ui/layout/notebook.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <wx/notebook.h>
+
+#include "ui/build.hpp"
+#include "ui/layout/priv/panel.hpp"
+#include "ui/detail/window.hpp"
+
+using namespace pcui;
+
+namespace {
+
+struct Layout : detail::Window<wxNotebook> {
+    Layout(const detail::Scaffold& scaffold, const Notebook& desc) {
+        Create(scaffold.childParent_, desc.win_.id_);
+        postCreation(scaffold, desc.win_);
+
+        for (auto& page : desc.pages_) {
+            auto *panel{new priv::Panel(this)};
+
+            build(panel, page.content_);
+
+            AddPage(panel, page.title_);
+        }
+
+        activate();
+    }
+};
+
+} // namespace
+
+DescriptorPtr Notebook::operator()() {
+    return std::make_unique<Notebook::Desc>(std::move(*this));
+}
+
+Notebook::Desc::Desc(Notebook&& data) :
+    Notebook{std::move(data)} {}
+
+wxSizerItem *Notebook::Desc::build(const detail::Scaffold& scaffold) const {
+    auto *split{new Layout(scaffold, *this)};
+
+    auto *item{new wxSizerItem(split)};
+    detail::apply(win_.base_, item);
+
+    return item;
+}
+
+detail::Descriptor *Notebook::Desc::clone() const {
+    return new Desc(*this);
+}
+
---
+++ src/components/ui/layout/notebook.hpp
@@ -0,0 +1,52 @@
+#pragma once
+/*
+ * ProffieConfig, All-In-One Proffieboard Management Utility
+ * Copyright (C) 2026 Ryan Ogurek
+ *
+ * components/ui/layout/notebook.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ui/detail/general.hpp"
+#include "ui/types.hpp"
+
+#include "ui_export.h"
+
+namespace pcui {
+
+struct UI_EXPORT Notebook {
+    struct Desc;
+
+    // TODO: Make this a base w/ C++ P2287.
+    detail::ChildWindowBase win_;
+
+    struct Page {
+        wxString title_;
+        pcui::DescriptorPtr content_;
+    };
+    std::vector<Page> pages_;
+
+    DescriptorPtr operator()();
+};
+
+struct UI_EXPORT Notebook::Desc : Notebook, detail::Descriptor {
+    Desc(Notebook&&);
+
+    [[nodiscard]] wxSizerItem *build(const detail::Scaffold&) const override;
+    [[nodiscard]] Descriptor *clone() const override;
+};
+
+} // namespace pcui
+
